ff0d7849bc205656bd34c0b8e93e7b7f0218c5e3
Jabonka <vicka.grebennickova@yandex.ru>
2023-03-02 12:46:17 +0300

    2labafull

--- main.js
+++ main.js
@@ -193,8 +193,6 @@ switch(a) {
             } 
         }
         function Randomazer(min,max) {
-            min = Math.ceil(min);
-            max = Math.floor(max);
             return Math.floor(Math.random() * (max - min + 1)) + min;
         }
         let answ=``;
