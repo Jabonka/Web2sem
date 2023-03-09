80ace3edbcb0d13d6b1e3df35f5e066e991efd8b
Jabonka <vicka.grebennickova@yandex.ru>
2023-03-09 13:06:23 +0300

    2 лаба

--- main.js
+++ main.js
@@ -216,3 +216,52 @@ switch(a) {
     //     a=+prompt(`Введите номер задания от 1 до 5`);
     //     break;
 }
+
+// function ShowPass(event){
+//     const showBtn = event.currentTarget;
+//     const form = showBtn.closest("form");
+//     const passInput = form.querySelector("[name='password']");
+
+//     if (passInput.type === "text") {
+//         passInput.type = "password";
+//     }
+//     else {
+//         passInput.type = "text";
+//     }
+// }
+
+// function sendForm(event) {
+//     event.preventDefault();
+
+//     const registerForm = event.currentTarget;
+//     const formData = new FormData(registerForm);
+
+//     console.table({
+//         "Email": formData.get("email"),
+//         "Пароль": formData.get("password"),
+//     });
+// }
+
+// const modal = document.getElementById("modal");
+// const btn = document.getElementById("btn");
+// const span = document.getElementsByClassName("close")[0];
+// const showpass=document.querySelector('.showpassword');
+
+// btn.onclick = function(){
+//     modal.style.display = "block";
+// }
+// span.onclick = function(){
+//     modal.style.display = "none";
+// }
+
+// window.onclick = function(event){
+//     if(event.target == modal){
+//         modal.style.display= "none";
+//     }
+// }
+
+// const registerForm = document.getElementById("reg");
+// registerForm.addEventListener("submit", sendForm);
+
+// showpass.addEventListener("pointerup", ShowPass);
+// showpass.addEventListener("pointerdown", ShowPass);
