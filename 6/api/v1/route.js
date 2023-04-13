e8f15437caf87d64b5e645ec9d59f18c61ab5873
Jabonka <vicka.grebennickova@yandex.ru>
2023-04-13 14:00:41 +0300

    4&6 labs

--- 6/api/v1/route.js
+++ 6/api/v1/route.js
@@ -1,5 +1,7 @@
 var express = require('express');
+const { Validation } = require('./middleware');
 var router = express.Router();
+
 
 let comments = [];
 let users = {}
@@ -8,46 +10,39 @@ router.get('/', function(req,res){
     res.send('Hello!');
 })
 router.get('/stats', function(req,res){
-    let data = ''
+    let data = req.body
     const name = req.headers['user-agent']
-    req.on('data', (chunk) => {
-        data += chunk
-    })
-    req.on('end', () => {
-        let firstHtml =
-            '<table>' +
-            '<tr>' +
-            '<td>Name</td>' +
-            '<td>Count request</td>' +
-            '</tr>'
-        let secondHtml = ''
+    let firstHtml =
+        '<table>' +
+        '<tr>' +
+        '<td>Name</td>' +
+        '<td>Count request</td>' +
+        '</tr>'
+    let secondHtml = ''
 
-        if (users[name]) {
+    if (users[name]) {
 
-            users[name] += 1
-        }else{
-            users[name] = 1
-        }
-        for (const key in users) {
-            secondHtml +=
-                `<tr>
-                    <td>${key}</td>
-                    <td>${users[key]}</td>
-                </tr>`
-        }
-        let resHtml = firstHtml + secondHtml + '</table>'
-        res.send(resHtml);
-    })
+        users[name] += 1
+    }else{
+        users[name] = 1
+    }
+    for (const key in users) {
+        secondHtml +=
+            `<tr>
+                <td>${key}</td>
+                <td>${users[key]}</td>
+            </tr>`
+    }
+    let resHtml = firstHtml + secondHtml + '</table>'
+    res.send(resHtml);
+    
 })
-router.post('/comments', function(req,res){
-    let data = ''
-    req.on('data', (chunk) => {
-        data += chunk
-    })
-    req.on('end', () => {
+router.post('/comments', Validation, function(req,res){
+    let data = req.body;
+    if (data){
         comments.push(data)
-        res.send(comments)
-    })
+    }
+    res.send(comments)    
 })
 
 module.exports = router;
--- 6/app.js
+++ 6/app.js
@@ -1,5 +1,8 @@
 var express=require('express');
 const router = require('./api/v1/route');
+const {Autorization, BadRequest, Morgan,Helmet} = require('./api/v1/middleware');
+const bodyParser=require('body-parser');
+var cowsay = require("cowsay");
 
 
 var app=express();
@@ -7,9 +10,24 @@ var app=express();
 const PORT=3000;
 const host='127.0.0.1';
 
-app.use(express.static(__dirname+'./public'));
 
-app.use('/',router);
+app.use(Helmet)
+app.use(Morgan)
+
+app.use(bodyParser.text());
+app.use(express.static('public'))
+
+console.log(cowsay.say({
+    text : "Muuuuu",
+    e : "O-",
+    T : "U "
+}));
+
+app.use('/api/v1', Autorization, router); //уровень маршрутизатора
+app.use(BadRequest) //уровень приложения
+
+
+
 app.listen(PORT, host, () => {
     console.log(`Server starting on ${host}:${PORT}`)
 })
